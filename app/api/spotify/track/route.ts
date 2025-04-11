ab595842a55f4e714ac9c10836e3b50d707b7818
oyuh <lawsonhart0919@gmail.com>
2025-04-11 09:02:31 -0500

    updated route for track maybe it should send correctly

--- app/api/spotify/track/route.ts
+++ app/api/spotify/track/route.ts
@@ -3,5 +3,7 @@ import { getNowPlaying } from '@/lib/spotify';
 
 export async function GET() {
   const track = await getNowPlaying();
-  return NextResponse.json(track || {});
+  const response = NextResponse.json(track || {});
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  return response;
 }
